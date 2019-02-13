68e9bacaf92843e5244e1f073890544edda2982b
Celyn Susanto <celyn.vy@gmail.com>
2019-02-13 14:58:58 +0700

    Celyn Susanto

--- server/app.js
+++ server/app.js
@@ -1,17 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose');
 const ArticleRoute = require('./routes/article')
 const cors = require('cors')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 mongoose
-    .connect('mongodb://localhost:27017/mini-wp', { useCreateIndex: true, useNewUrlParser: true })
+    .connect(process.env.DATABASE, { useCreateIndex: true, useNewUrlParser: true })
     .then(() => {
         console.log("Successfully Connected to Database")
     })
